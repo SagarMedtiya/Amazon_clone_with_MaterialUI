4f252c54abe453eb9afa3da05f5fe40e6d1dadb0
SagarMedtiya <sagarmedtiya31@gmail.com>
2021-11-14 21:46:19 +0530

    edit checkout

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import './CheckProduct.css'
-import Button from '@material-ui/core/Button';
-import { useStateValue } from './StateProvider';
-import Rating from '@material-ui/lab/Rating';
-
+import React from "react";
+import "./CheckProduct.css";
+import Button from "@material-ui/core/Button";
+import { useStateValue } from "./StateProvider";
+import Rating from "@mui/material/Rating";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
-    const [{basket}, dispatch] = useStateValue();
-    const removeFromBasket = () =>{
-        dispatch({
-            type: 'REMOVE_FROM_BASKET',
-            id:id
-        })
-    }
-    return (
-        <div className='checkoutProduct'>
-            <img className="checkoutProduct__img"src={image} />
-            <div className="checkoutProduct__info">
-                <p className='checkoutProduct__title'>{title}</p>
-                <p className='checkoutProduct__price'>
-                    <small>₹</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className="checkoutProduct__rating">
-                <Rating name="half-rating" defaultValue={rating} precision={0.5} />
-                </div>
-                <Button variant="text" color="default" onClick={removeFromBasket}>
-                  Delete
-                </Button>
-            </div>
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__img" src={image} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>₹</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          <Rating
+            name="simple-controlled"
+            value={rating}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          />
         </div>
-    )
+        <Button variant="text" color="default" onClick={removeFromBasket}>
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutProduct
+export default CheckoutProduct;
